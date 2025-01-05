1309c7a0e332f074d2d2903b6fc22703a25976a4
James Jelenko <jamesjelenko@gmail.com>
2025-01-05 05:19:16 -0800

    add simple timer helper utility

---
+++ Timer.cs
@@ -0,0 +1,27 @@
+public Program()
+{
+    Runtime.UpdateFrequency = UpdateFrequency.Update10;
+}
+
+public bool IsLocal(IMyCubeBlock block) 
+{
+   bool local = block.CubeGrid.EntityId == Me.CubeGrid.EntityId;
+    return local;
+}
+
+public void HandleLightSequence(int interval, List<IMyLightingBlock> lights) 
+{
+    var idx = DateTime.UtcNow.Second % lights.Count;
+    var next = (idx + 1) % lights.Count();
+
+    lights[idx].Enabled = false;
+    lights[next].Enabled = true;    
+}
+
+public void Main()
+{
+    
+    List<IMyLightingBlock> lights = new List<IMyLightingBlock>();
+    GridTerminalSystem.GetBlocksOfType(lights, light => IsLocal(light) && light.CustomName.Contains("Corner Light"));
+
+}
