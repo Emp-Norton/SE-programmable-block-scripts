db10bdfbd2602828f903f5a5c4b289e7ab17e58a
James Jelenko <jamesjelenko@gmail.com>
2025-01-04 23:18:06 -0800

    Refactor status display utility for multiple gasses

--- EmpNort.PbScriptProj/Program.cs
+++ EmpNort.PbScriptProj/Program.cs
@@ -41,25 +41,44 @@ namespace IngameScript
 
         public void Main(string argument, UpdateType updateSource)
         {
+            var onlyGridOwned = argument.Contains("onlyGridOwned");
+            
+            // Finder: Create battery list and LCD ref
             var batteries = new List<IMyBatteryBlock>();
             GridTerminalSystem.GetBlocksOfType(batteries);
             IMyTextPanel batteryPanel = GridTerminalSystem.GetBlockWithName("batteryLcd") as IMyTextPanel;
 
-            List<IMyGasTank> tanks = new List<IMyGasTank>();
-            GridTerminalSystem.GetBlocksOfType(tanks, tank => tank.BlockDefinition.SubtypeName.Contains("HydrogenTank"));
+            // Finder: Create hydrogen list and filter
+            List<IMyGasTank> hydrogenTanks = new List<IMyGasTank>();
+            GridTerminalSystem.GetBlocksOfType(hydrogenTanks, tank => tank.BlockDefinition.SubtypeName.Contains("HydrogenTank") && (onlyGridOwned ? tank.CubeGrid.EntityId == Me.CubeGrid.EntityId : true));
 
-            IMyTextPanel hydroPanel = GridTerminalSystem.GetBlockWithName("hydroLcd") as IMyTextPanel;
+            // Finder: Create oxygen list and filter
+            List<IMyGasTank> oxygenTanks = new List<IMyGasTank>();
+            GridTerminalSystem.GetBlocksOfType(oxygenTanks, tank => tank.DetailedInfo.Contains("Oxygen") && (onlyGridOwned ? tank.CubeGrid.EntityId == Me.CubeGrid.EntityId : true));
 
+            // Finder: Create gas status LCD ref
+            IMyTextPanel gasStatusPanel = GridTerminalSystem.GetBlockWithName("hydroLcd") as IMyTextPanel;
+
+            // Finder: Get Inventories
             List<IMyTerminalBlock> inventories = new List<IMyTerminalBlock>();
             GridTerminalSystem.GetBlocksOfType<IMyTerminalBlock>(inventories, block => block.HasInventory);
 
-            if (hydroPanel != null && tanks.Count > 0 && inventories.Count > 0)
+            if (gasStatusPanel != null && hydrogenTanks.Count > 0 && inventories.Count > 0)
             {
-                _utility.ShowHydrogen(hydroPanel, tanks, inventories);
+                _utility.ShowHydrogen(gasStatusPanel, hydrogenTanks, inventories);
             }
             else
             {
-                Echo($"Unable to locate Hydrogen LCD or required components.");
+                Echo($"Unable to find Gas LCD, is it null: {gasStatusPanel == null}, or HydrogenTanks: #{hydrogenTanks.Count}.");
+            }
+
+            if (gasStatusPanel != null && oxygenTanks.Count > 0 && inventories.Count > 0)
+            {
+                _utility.ShowOxygen(gasStatusPanel, oxygenTanks, inventories);
+            }
+            else
+            {
+                Echo($"Unable to find Gas LCD, is it null: {gasStatusPanel == null},  or OxygenTanks: #{oxygenTanks.Count}");
             }
 
             if (batteryPanel != null && batteries.Count > 0)
@@ -82,6 +101,37 @@ namespace IngameScript
             // Call the enemy detection utility
             _enemyDetection.DetectEnemiesFromAntennae(lcd, antennas, lightGroup);
 
+            string lcdName = "turretsLcd";
+
+            // Get the LCD panel
+            IMyTextPanel turretLcd = GridTerminalSystem.GetBlockWithName(lcdName) as IMyTextPanel;
+            if (lcd == null)
+            {
+                Echo($"Error: LCD panel '{lcdName}' not found.");
+                return;
+            }
+
+            // Create utility instance
+            TurretStatusUtility turretUtility = new TurretStatusUtility(GridTerminalSystem);
+
+            // Get turret status
+            string turretStatus = turretUtility.GetTurretStatus();
+
+            // Update LCD content
+            lcd.ContentType = ContentType.TEXT_AND_IMAGE;
+            lcd.WriteText(turretStatus);
+
+            // Set LCD color based on ammo status
+            if (turretUtility.IsAnyTurretEmpty())
+            {
+                turretLcd.FontColor = Color.Red;
+            }
+            else
+            {
+                turretLcd.FontColor = Color.White;
+            }
+
+            Echo("Turret ammo status updated.");
 
 
 
