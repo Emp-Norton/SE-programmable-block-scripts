389d95af1d14944612ced55459b212789d446705
James Jelenko <jamesjelenko@gmail.com>
2025-01-08 00:13:55 -0800

    add raycast ad-hoc script to project mixin

--- Mdk.PbRaycastMixin/Class1.cs
+++ Mdk.PbRaycastMixin/Class1.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
+using System.Net.Mime;
 using System.Text;
 using Sandbox.Game.EntityComponents;
 using Sandbox.ModAPI.Ingame;
@@ -20,7 +21,50 @@ using VRageMath;
 
 namespace IngameScript
 {
-    internal class UtilityClass
+    internal class RaycastUtility
     {
+        public RaycastUtility() 
+        {
+            void SetupCast(double range, IMyCameraBlock camera, IMyTextPanel lcd)
+            {
+                
+                Echo($"Range: {camera.AvailableScanRange.ToString()}");
+                // Ensure the camera is enabled for raycasting
+                camera.EnableRaycast = true;
+
+               // IMySoundBlock sound = GridTerminalSystem.GetBlockWithName("Sound Block") as IMySoundBlock;
+
+                //if (camera.AvailableScanRange >= (scanRange * 0.8)) { sound.Play(); } else { sound.Stop(); }
+                if (camera.AvailableScanRange >= scanRange)
+                {
+                   // sound.Play();
+                    // Perform the scan
+                    MyDetectedEntityInfo target = camera.Raycast(scanRange);
+
+                    // Prepare display text
+                    string output;
+                    if (!target.IsEmpty())
+                    {
+                        output = $"Camera Output\n\n" +
+                                 $"Entity Detected:\n" +
+                                 $"- Name: {target.Name}\n" +
+                                 $"- Type: {target.Type}\n" +
+                                 $"- Distance: {Vector3D.Distance(camera.GetPosition(), target.Position):F1} m\n" +
+                                 $"- Position: {target.Position}";
+                    }
+                    else
+                    {
+                        output = "Camera Output\n\nNo entity detected within range.";
+                    }
+
+                    // Write to LCD
+                    lcd.ContentType = ContentType.TEXT_AND_IMAGE;
+                    lcd.WriteText(output);
+
+                    // Echo for in-game programmable block status
+                    Echo("Camera scan complete.");
+                }
+            }
+        }
     }
 }
